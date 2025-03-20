fe00951388b084777b7de3268575aa2558441bf9
F8Y <batigoal277@gmail.com>
2025-03-20 18:03:40 +1000

    Объект для списков на странице Programming

---
+++ frontend/src/Pages/programming/ui/nav/navData.js
@@ -0,0 +1,19 @@
+
+export const navData = [
+    {
+        title: 'Web programming',
+        items: ['JavaScript', 'FrameWorks', 'Golang', 'Ruby'],
+    },
+    {
+        title: 'Object-oriented programming',
+        items: ['C++', 'Rust', 'Java', 'C#'],
+    },
+    {
+        title: 'Data analyzing',
+        items: ['Fortran', 'Prolog', 'Python'],
+    },
+    {
+        title: 'Mobile programming',
+        items: ['Kotlin', 'Swift', 'Objective C'],
+    },
+];
