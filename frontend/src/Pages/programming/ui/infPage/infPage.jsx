ced09d6098842b7e409e85d2f7d5bcd00a66ea49
F8Y <batigoal277@gmail.com>
2025-04-01 00:15:19 +1000

    Создана пустая страница для контента ЯП'ов

---
+++ frontend/src/Pages/programming/ui/infPage/infPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from './infPage.module.scss'
+import programmingData from '../../programming.json'
+import PropTypes from 'prop-types';
+
+export const InfPage = () => {
+    return (
+        <div>
+            <h1>aboba</h1>
+        </div>
+    );
+};
+
