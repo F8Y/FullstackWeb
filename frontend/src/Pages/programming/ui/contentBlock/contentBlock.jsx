43aef44f5e39a6f197b34313bb8fb627d90d6466
F8Y <batigoal277@gmail.com>
2025-04-01 00:14:06 +1000

    Стили, линк к контенту и фикс структуры для страницы с ЯП'ами

--- frontend/src/Pages/programming/ui/contentBlock/contentBlock.jsx
+++ frontend/src/Pages/programming/ui/contentBlock/contentBlock.jsx
@@ -1,16 +1,15 @@
-// contentBlock.jsx
 import s from './contentBlock.module.scss';
 import programmingData from '../../programming.json';
 import PropTypes from 'prop-types';
+import { NavLink } from "react-router-dom";
 
 export const ContentBlock = ({ category, language }) => {
-    // Пытаемся получить данные категории
+
     const categoryData = programmingData.data[category];
     if (!categoryData) {
         return <div>Нет данных для категории "{category}"</div>;
     }
 
-    // Получаем массив блоков по выбранному языку
     const contentBlocks = categoryData[language];
     if (!contentBlocks) {
         return <div>Контент для "{language}" в категории "{category}" отсутствует</div>;
@@ -20,21 +19,27 @@ export const ContentBlock = ({ category, language }) => {
         <div className={s['contentBlock']}>
             {contentBlocks.map((block, index) => (
                 <div key={index} className={s['contentBlock__block']}>
-                    <h2>{block.title}</h2>
+                    {block.image && (
+                        <NavLink to="/inf">
+                            <img src={block.image} alt={block.title} />
+                        </NavLink>
+                    )}
+                    <div className={s['contentBlock__block--text']}>
                     <h3>{block.subtitle}</h3>
+                    <h2>{block.title}</h2>
                     <p>{block.description}</p>
-                    {block.image && <img src={block.image} alt={block.title} />}
+                    </div>
 
-                    {block.content && block.content.map((item, idx) => (
-                        <div key={idx} className={s['contentBlock__block--content']}>
-                            {item.paragraph && <p>{item.paragraph}</p>}
-                            {item.resourceName && item.url && (
-                                <a href={item.url} target="_blank" rel="noopener noreferrer">
-                                    {item.resourceName}
-                                </a>
-                            )}
-                        </div>
-                    ))}
+                    {/*{block.content && block.content.map((item, idx) => (*/}
+                    {/*    <div key={idx} className={s['contentBlock__block--content']}>*/}
+                    {/*        {item.paragraph && <p>{item.paragraph}</p>}*/}
+                    {/*        {item.resourceName && item.url && (*/}
+                    {/*            <a href={item.url} target="_blank" rel="noopener noreferrer">*/}
+                    {/*                {item.resourceName}*/}
+                    {/*            </a>*/}
+                    {/*        )}*/}
+                    {/*    </div>*/}
+                    {/*))}*/}
                 </div>
             ))}
         </div>
