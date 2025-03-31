b1f8caf324217d4e4eb3febe71118df6b7d672ce
F8Y <batigoal277@gmail.com>
2025-04-01 00:14:38 +1000

    Подстветка для активных линков

--- frontend/src/Widgets/header/header.jsx
+++ frontend/src/Widgets/header/header.jsx
@@ -1,22 +1,64 @@
-import {NavLink} from "react-router-dom";
-import s from './header.module.scss'
-import '../../App/styles/global.scss'
-import festuLogo from '../../img/assets/festuLogo.png'
+import { NavLink } from "react-router-dom";
+import s from './header.module.scss';
+import '../../App/styles/global.scss';
+import festuLogo from '../../img/assets/festuLogo.png';
 
 export const Header = () => {
     return (
-        <header className={s['header']}>
+        <header className={s.header}>
             <NavLink to={'/'}>
-            <img className={s['header__image']} src={festuLogo} alt="Логотип"/>
+                <img className={s['header__image']} src={festuLogo} alt="Логотип" />
             </NavLink>
             <nav className={s['header__navigationBar']}>
                 <ul className={s['header__navigationBar--list']}>
-                    <li><NavLink to="/">HOME</NavLink></li>
-                    <li><NavLink to="/faq">FAQ</NavLink></li>
-                    <li><NavLink to="/schedule">SCHEDULE</NavLink></li>
-                    <li><NavLink to="/disciplines">DISCIPLINES</NavLink></li>
-                    <li><NavLink to="/programming">PROGRAMMING</NavLink></li>
-                    <li><NavLink to="/yagpt">YaGPT</NavLink></li>
+                    <li>
+                        <NavLink
+                            to="/"
+                            className={({ isActive }) => isActive ? s.activeLink : undefined}
+                        >
+                            HOME
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/faq"
+                            className={({ isActive }) => isActive ? s.activeLink : undefined}
+                        >
+                            FAQ
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/schedule"
+                            className={({ isActive }) => isActive ? s.activeLink : "fff"}
+                        >
+                            SCHEDULE
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/disciplines"
+                            className={({ isActive }) => isActive ? s.activeLink : undefined}
+                        >
+                            DISCIPLINES
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/programming"
+                            className={({ isActive }) => isActive ? s.activeLink : undefined}
+                        >
+                            PROGRAMMING
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/yagpt"
+                            className={({ isActive }) => isActive ? s.activeLink : undefined}
+                        >
+                            YaGPT
+                        </NavLink>
+                    </li>
                 </ul>
             </nav>
         </header>
