047b5b7c793870b01d930579dd65b5715a4f74f8
F8Y <batigoal277@gmail.com>
2025-04-09 18:58:00 +1000

    Руты через лэйауты

--- frontend/src/routes/appRoutes.jsx
+++ frontend/src/routes/appRoutes.jsx
@@ -1,24 +1,28 @@
-import {Routes, Route} from "react-router-dom";
-import {HomePage} from "../Pages/home/homePage.jsx";
-import {FaqPage} from "../Pages/faq/faqPage.jsx";
-import {SchedulePage} from "../Pages/schedule/schedulePage.jsx";
+import { Routes, Route } from "react-router-dom";
+import { HomePage } from "../Pages/home/homePage.jsx";
+import { FaqPage } from "../Pages/faq/faqPage.jsx";
+import { SchedulePage } from "../Pages/schedule/schedulePage.jsx";
 import { ProgrammingPage } from "../Pages/programming/programmingPage.jsx";
 import { InfPage } from "../Pages/programming/ui/infPage/infPage.jsx";
-import PropTypes from "prop-types";
+import { ErrorPage } from "../Pages/error/errorPage.jsx";
 
-export const AppRoutes = ({scrollY}) => {
+import { MainLayout } from "../layouts/MainLayout.jsx";
+import { BareLayout } from "../layouts/BareLayout.jsx";
+
+export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path = "/" element = {<HomePage scrollY={scrollY}/>} />
-            <Route path = '/faq' element = {<FaqPage />} />
-            <Route path = '/schedule' element = {<SchedulePage />} />
-            <Route path = '/programming' element={<ProgrammingPage />} />
-            <Route path = '/inf' element = {<InfPage />} />
-            <Route path = "*" element = {<HomePage scrollY={scrollY}/>} />   {/*Добавить pageNotFound*/}
+            <Route element={<MainLayout />}>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/faq" element={<FaqPage />} />
+                <Route path="/schedule" element={<SchedulePage />} />
+                <Route path="/programming" element={<ProgrammingPage />} />
+                <Route path="/inf" element={<InfPage />} />
+            </Route>
+
+            <Route element={<BareLayout />}>
+                <Route path="*" element={<ErrorPage />} />
+            </Route>
         </Routes>
     );
-}
-
-AppRoutes.propTypes = {
-    scrollY : PropTypes.number.isRequired
 };
