48d0c80dfa94e352165be79572c43e8faff3740e
F8Y <batigoal277@gmail.com>
2025-04-09 18:57:36 +1000

    App рендерит только руты, вся остальная логика в лэйаутах

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -1,34 +1,5 @@
-import { useState, useEffect } from "react";
-import { Header } from "../Widgets/header/header.jsx";
-import { Footer } from "../Widgets/footer/footer.jsx";
 import { AppRoutes } from "../routes/appRoutes.jsx";
 
-
 export const App = () => {
-    const [scrollY, setScrollY] = useState(0);
-
-    useEffect(() => {
-        const handleScroll = () => {
-            setScrollY(window.scrollY);
-        };
-
-        window.addEventListener("scroll", handleScroll);
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
-
-    useEffect(() => {
-        document.documentElement.style.setProperty("--scrollY", scrollY + "px");
-    }, [scrollY]);
-
-    return (
-        <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh", }}>
-            <Header/>
-            <main style={{ flex: 1 }}>
-            <AppRoutes scrollY={scrollY} />
-            </main>
-            <Footer/>
-        </div>
-    );
+    return <AppRoutes />;
 };
